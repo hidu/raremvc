31813b9d0d2dbccb7084cccb92c41a367fdc5c02
duv123 <duv123@efd73241-b8cb-3ec4-3ed7-0fca8939d68d>
2011-02-28 10:55:24 +0000

    修改shell脚本

--- rare/init.sh
+++ rare/init.sh
@@ -53,8 +53,8 @@ class myFilter{
 ">lib/myFilter.class.php
 
 ### default layout
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+echo '<!DOCTYPE html>
+<html>
 <head>
 <?php rareView::include_title();?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
