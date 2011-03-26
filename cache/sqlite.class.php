8cf9e12d5790084abea657b72655de49dc488355
duv123 <duv123@gmail.com>
2011-03-26 07:04:01 +0000

    fix sqlite cache

--- cache/sqlite.class.php
+++ cache/sqlite.class.php
@@ -9,17 +9,17 @@ class rCache_sqlite extends rCache{
     /**
      * @param string $cacheMod 缓存级别 默认为当前全局
      */
-    public function __construct($cacheMod='root'){
+    public function __construct($dbName='cache',$cacheMod='root'){
         $filename=$cacheMod=='app'?RARE_CACHE_DIR:dirname(RARE_CACHE_DIR)."/";
         
-        $this->db=new PDo("sqlite:".$filename."cache.sqlite");
+        $this->db=new PDo("sqlite:".$filename.$dbName.".sqlite");
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $q=@$this->db->query("select * from cache limit 1");
         if($q==false){
-         $this->db->exec("create table cache(id varchar(255),data text,life int);CREATE UNIQUE INDEX [cache_unique] ON cache ([key])"); 
+         $this->db->exec("create table cache(id varchar(255),data text,life int,mtime int);CREATE UNIQUE INDEX [cache_unique] ON cache ([id])"); 
         }
         if(mt_rand(0, 100)==50){
-           $this->db->exec("delete from cache where life>0 and life<".time());
+           $this->db->exec("delete from cache where life>0 and life<".time()-1800);
         }
     }   
   
@@ -40,8 +40,8 @@ class rCache_sqlite extends rCache{
     
     public function set($key, $data,$lifetime=null){
       if(!is_null($lifetime))$lifetime+=time();
-      $sth=$this->db->prepare("insert or replace into cache(id,data,life) values(?,?,?)");
-      return  $sth->execute(array($key,$data,$lifetime));
+      $sth=$this->db->prepare("insert or replace into cache(id,data,life,mtime) values(?,?,?,?)");
+      return  $sth->execute(array($key,$data,$lifetime,time()));
     }
     
     public function remove($key){
