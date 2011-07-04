9691088225c5f179e2451f18965d1a4ac1aa9c0e
duv123 <duv123@gmail.com>
2011-07-04 08:16:35 +0000

    sqlite listPage

--- db/driver/sqlite.class.php
+++ db/driver/sqlite.class.php
@@ -1,4 +1,12 @@
 <?php
 class rdb_driver_sqlite{
-   
+       public static function listPage($sql,$params=array(),$size=10,$dbName=null,$page=1){
+        $start= $size*($page-1);
+        $limit= $start.','.$size;
+        $sqlLimit=$sql." LIMIT ".$limit;
+        $list=rDB::queryAll($sqlLimit, $params,$dbName);
+        $sql2=preg_replace("/select*\s+from/i", "SELECT count(*) FROM ", $sql);
+        $total=(int)rDB::execQuery($sql2,array(),$dbName)->fetchColumn();
+        return array($list,$total);
+   } 
 }
