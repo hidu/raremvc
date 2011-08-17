fac7afa7e6fb4cf868b199af023da84f803c2401
duv123 <duv123@gmail.com>
2011-08-17 09:26:10 +0000

    优化sqlite cache

--- cache/object.class.php
+++ cache/object.class.php
@@ -4,20 +4,39 @@
  *@author duwei
  *对象缓存，缓存的数据会自动进行虚拟化处理、取出时进行反虚拟化
  */
-class rCache_object extends rCache_file{
+class rCache_object extends rCache{
+   /**
+    * @var rCache
+    */
+   private $handle;
+   
     public function __construct(){
-       parent::__construct();
+       $this->handle=new rCache_sqlite("object");
+     }
+     
+    public function setHandle($handle){
+       $this->handle=$handle;
+     }
+    
+    
+    public function has($key){
+      return $this->handle->has($key);
     }
     
    public function get($key,$default=null){
-      $file=$this->getCacheFilePath($key);
-      if(!file_exists($file))return $default;
-      $data=parent::get($key);
-      $data=unserialize($data);
-      return $data===false?$default:$data;
+     if($this->handle->has($key)){
+        $data=$this->handle->get($key);
+        return unserialize($data);
+     }else{
+        return $default;
+     }
    }
    
    public function set($key,$data,$lifetime=0){
-      return parent::set($key, serialize($data),$lifetime);
+      return $this->handle->set($key, serialize($data),$lifetime);
+   }
+   
+   public function remove($key){
+     $this->handle->remove($key);
    }
 }
