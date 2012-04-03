bf85f03c28d26840e100195aca6d547344cec9f3
duv123 <duv123@gmail.com>
2012-04-03 06:34:41 +0000

    use rdao

--- lib/dao/article.class.php
+++ lib/dao/article.class.php
@@ -1,24 +1,16 @@
 <?php
-class dao_article extends dao_base{
-  public function __construct(){
-     $this->tableName="article";
-     $this->keyField="articleid";
+class dao_article extends rDao_base{
+  
+  public function init(){
+     $this->table_name="article";
+     $this->key_field="articleid";
   }
   
-  public function updateByField($fieldName, $fieldValue, $data){
+  public function onUpdate($data, $where, $params){
     $data['mtime']=time();
-    return parent::updateByField($fieldName, $fieldValue, $data);
   }
   
-  public function add($data){
+ public function onInsert($data){
      $data['ctime']=$data['mtime']=time();
-     return parent::add($data);
-  }
-  public function save($data){
-    if(empty($data[$this->keyField])){
-       $data['ctime']=$data['mtime']=time();
-       unset($data[$this->keyField]);
-    }
-    return parent::save($data);
-  }
+ }
 }
