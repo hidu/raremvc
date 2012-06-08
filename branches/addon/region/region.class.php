8c01fd43b65db90b9869e53ea6c3ca39e1e29d19
duv123@gmail.com <duv123@gmail.com@efd73241-b8cb-3ec4-3ed7-0fca8939d68d>
2012-06-08 10:30:57 +0000

    region

---
+++ branches/addon/region/region.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 地域相关功能
+ * @author duwei
+ *
+ */
+class rRegion{
+    
+    private static function load($name){
+        static $file_data=array();
+        if(!isset($file_data[$name])){
+            $file_data[$name]=require dirname(__FILE__)."/data/".$name;
+          } 
+         return   $file_data[$name];
+    }
+    
+    public static function getAllProvince(){
+        return self::load("prov.php");
+    }
+    
+    public static function getCitiesByProvinceId($pid){
+        $allCities=self::load("city.php");
+        return isset($allCities[$pid])?$allCities[$pid]:array();
+    }
+    
+    public static function getCountiesByCityId($cityId){
+        $allCounties=self::load("county.php");
+        return isset($allCounties[$cityId])?$allCounties[$cityId]:array();
+    }
+    
+    public static function getProvinceName($pid){
+        $all=self::getAllProvince();
+        return isset($all[$pid])?$all[$pid]:null;
+    }
+    
+    
+    public static function getCityNameByCityId($cityId){
+       $cities=self::getCitiesByProvinceId(substr($cityId."", 0,2)."0000");
+       return isset($cities[$cityId])?$cities[$cityId]:null;  
+    }
+}
