9fe82a54a87e1a2c71a1945a9cb545d80f49eff7
duv123@gmail.com <duv123@gmail.com@efd73241-b8cb-3ec4-3ed7-0fca8939d68d>
2012-06-25 09:24:27 +0000

    postgresql pagerlimit

--- branches/addon/db/driver/postgresql.class.php
+++ branches/addon/db/driver/postgresql.class.php
@@ -1,12 +1,11 @@
 <?php
 class rdb_driver_postgresql{
    public static function setEncode($encode,$pdo){
-     // $pdo->exec("SET NAMES $encode");
+      $pdo->exec("SET NAMES $encode");
   }
    public static function listPage($sql,$param,$size=10,$page=1,$pdo=null){
         $start= $size*($page-1);
-        $limit= $start.','.$size;
-        $sqlLimit=$sql." LIMIT ".$limit;
+        $sqlLimit=$sql." LIMIT {$size} OFFSET ".$start;
         $list=$pdo->query($sqlLimit)->fetchAll();
         
         $sql2=preg_replace("#^select\s+.*\sfrom\s#i", "SELECT count(*) FROM ", $sql);
