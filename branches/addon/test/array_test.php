048027f86852ca57faf1c908099dfd3662a31efc
duv123@gmail.com <duv123@gmail.com@efd73241-b8cb-3ec4-3ed7-0fca8939d68d>
2012-12-03 06:31:30 +0000

    array filter support many function

--- branches/addon/test/array_test.php
+++ branches/addon/test/array_test.php
@@ -33,39 +33,44 @@ class array_test extends PHPUnit_Framework_TestCase{
     }
     
     public function test_filter(){
-//        $result= rArray::filter($this->arr, "(id>=1 and id<2) and name=aaa or id.2=c or id=='4'");
-//        $a1=array(0,3,6);
-//       $this->assertEquals($a1, array_keys($result));
-//       $result= rArray::filter($this->arr, "名字=rArray数组");
-//       $this->assertEquals(array(6), array_keys($result));
+       $result= rArray::filter($this->arr, "(id>=1 and id<2) and name=aaa or id.2=c or id=='4'");
+       $a1=array(0,3,6);
+      $this->assertEquals($a1, array_keys($result));
+      $result= rArray::filter($this->arr, "名字=rArray数组");
+      $this->assertEquals(array(6), array_keys($result));
       
-//       $result=rArray::filter($this->arr, " id in ('1',2)");
-//       $this->assertEquals(2, count($result));
-//       $this->assertEquals(1, $result[0]['id']);
-//       $this->assertEquals(2, $result[1]['id']);
+      $result=rArray::filter($this->arr, " id in ('1',2)");
+      $this->assertEquals(2, count($result));
+      $this->assertEquals(1, $result[0]['id']);
+      $this->assertEquals(2, $result[1]['id']);
       
-//       $result=rArray::filter($this->arr, " id not in ('1',2, 3) and name not in(ddd,eee)");
-// //       print_r($result);
-//       $this->assertEquals(2, count($result));
-//       $this->assertEquals(4, $result[3]['id']);
-//       $this->assertEquals(5, $result[4]['i']);
       
-//       $result=rArray::filter($this->arr, "名字 in( rArray数组)");
-//       $this->assertEquals($this->arr[6], $result[6]);
-//       $this->assertEquals(1, count($result));
+      $result=rArray::filter($this->arr, " id not in ('1',2, 3) and name not in(ddd,eee)");
+//       print_r($result);
+      $this->assertEquals(2, count($result));
+      $this->assertEquals(4, $result[3]['id']);
+      $this->assertEquals(5, $result[4]['i']);
       
-//       $result=rArray::filter($this->arr, "名字 in ( \"rArray数组\")");
-//       $this->assertEquals($this->arr[6], $result[6]);
-//       $this->assertEquals(1, count($result));
+      $result=rArray::filter($this->arr, "名字 in( rArray数组)");
+      $this->assertEquals($this->arr[6], $result[6]);
+      $this->assertEquals(1, count($result));
       
-//       $result=rArray::filter($this->arr, "名字 in( 'rArray数组')");
-//       $this->assertEquals($this->arr[6], $result[6]);
-//       $this->assertEquals(1, count($result));
+      $result=rArray::filter($this->arr, "名字 in ( \"rArray数组\")");
+      $this->assertEquals($this->arr[6], $result[6]);
+      $this->assertEquals(1, count($result));
+      
+      $result=rArray::filter($this->arr, "名字 in( 'rArray数组')");
+      $this->assertEquals($this->arr[6], $result[6]);
+      $this->assertEquals(1, count($result));
       
       $result=rArray::filter($this->arr, "id!=1 and id<3");
       $this->assertEquals($this->arr[1], $result[1]);
       $this->assertEquals(1, count($result));
-      
+
+      $result= rArray::filter($this->arr, "(id>=1 and id<2) and name=aaa  or substr(name,1,1)=d");
+     $this->assertEquals(6,$result[5]['i']);
+     $this->assertEquals(1,$result[0]['i']);
+     $this->assertEquals(2,count($result));
 //       try{
 //         $result=rArray::filter($this->arr, "名字 in( 'rArray数组') hello");
 //       }catch(Exception $e){}
@@ -73,6 +78,7 @@ class array_test extends PHPUnit_Framework_TestCase{
 //      preg_match_all("/(\S+)\s?=\s?(\S+)\s/", " 名字1.1 = rArray asdasd",$matches);
 //      preg_match_all("/\s(\S+?)\s*([>=<]={0,2})\s*[\"']?(.+?)[\"']?\s/", " 名字1.1 = rArray asdasd",$matches);
 //      print_r($matches);
+
     }
     
     public function test_toTree(){
@@ -90,3 +96,5 @@ class array_test extends PHPUnit_Framework_TestCase{
        $this->assertEquals(2, $result[1]['id']);
     }
 }
+// $a=new array_test();
+// $a->test_filter();
